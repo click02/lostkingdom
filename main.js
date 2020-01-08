64ca6a23ba2ea4bd356d3fcd36a7c0e9e0fd68b9
sda782 <59613370+sda782@users.noreply.github.com>
2020-01-08 16:16:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var data;
+var players = [];
 
 $(document).ready(function() {
-
+    //  getting player online
     $.getJSON('https://api.minetools.eu/ping/fallen.serverminer.com/', function(api) {
         if (api.players.sample != null) {
             api.players.sample.forEach(element => {
                 var img = document.createElement("img");
-                $('#players_online').append('<div><img src="https://minotar.net/helm/' + element.id + '/100.png"><p>' + element.name + '</p></div>');
+                $('#players_online').append('<div><img src="https://minotar.net/helm/' + element.id + '/150.png"><p>' + element.name + '</p></div>');
             });
         } else {
             $('#players_online').append('<div><p>no one is online right now</p></div>');
@@ -14,4 +14,5 @@ $(document).ready(function() {
         document.getElementById("loading_screen").style.display = "none";
     });
 
+    //  getting discord ranking
 });
