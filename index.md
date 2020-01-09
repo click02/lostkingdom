8541cc550cc5d5d58e1c6a3ad97642209d6a5853
sda782 <59613370+sda782@users.noreply.github.com>
2020-01-09 11:33:25 +0100

    updated pretty much everything

--- index.md
+++ index.md
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lost Kingdom </title>
+    <title>The Lost Kingdom </title>
     <!-- getting fonts from google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Lobster|Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -14,17 +14,11 @@
 </head>
 
 <body>
-    <div id="loading_screen"></div>
+
     <header>
         <h1>The Lost Kingdom</h1>
-        <h3>unoffical website for the lost kingdom</h3>
+        <h3>But why</h3>
     </header>
-    <!--
-    <nav>
-        <a href="">Welcome</a>
-        <a href="">Player Online</a>
-        <a href="">Server Status</a>
-    </nav>-->
     <section>
         <div id="landing_page">
             <h1>Welcome</h1>
@@ -32,16 +26,17 @@
         </div>
         <div id="server_status">
             <h1>Players online</h1>
+            <div id="loading_screen"></div>
             <p>See who's online right now?</p>
             <div id="players_online">
 
             </div>
         </div>
-
-
     </section>
 
-    <footer></footer>
+    <footer>
+        <p>created by saernda &copy; 2019</p>
+    </footer>
     <script src="main.js"></script>
 </body>
 
--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-var players = [];
-
 $(document).ready(function() {
-    //  getting player online
-    $.getJSON('https://api.minetools.eu/ping/fallen.serverminer.com/', function(api) {
-        if (api.players.sample != null) {
-            api.players.sample.forEach(element => {
-                var img = document.createElement("img");
-                $('#players_online').append('<div><img src="https://minotar.net/helm/' + element.id + '/150.png"><p>' + element.name + '</p></div>');
-            });
-        } else {
-            $('#players_online').append('<div><p>no one is online right now</p></div>');
-        }
+    //  getting color rank for all players
+    $.getJSON('https://raw.githubusercontent.com/sda782/sda782.github.io/master/colorDatabase.json', function(colordata) {
+
+        //  getting online players
+        $.getJSON('https://api.minetools.eu/ping/fallen.serverminer.com/', function(api) {
+            if (api.players.sample != null) {
+                api.players.sample.forEach(element => {
+                    var color = "white";
+                    $.each(colordata, function(player, value) {
+                        if (element.name == value.name) {
+                            color = value.color;
+                            console.log(element.name + ' element');
+                            console.log(value.name + ' value');
+                        }
+
+                    });
+                    console.log(color);
+                    $('#players_online').append('<div style="background-color:' + color + ';"><img src="https://minotar.net/helm/' + element.id + '/150.png" ><p >' + element.name + '</p></div>');
+                });
+            }
+
+        });
         document.getElementById("loading_screen").style.display = "none";
     });
-
-    //  getting discord ranking
 });
